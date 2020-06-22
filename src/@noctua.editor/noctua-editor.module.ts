a728adb137df034561ab8fb16fed23581d1103e3
tmushayahama <tmushayahama@gmail.com>
2020-06-21 20:09:52 -0700

    added the inline with initials

--- src/@noctua.editor/noctua-editor.module.ts
+++ src/@noctua.editor/noctua-editor.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { NoctuaSharedModule } from '@noctua/shared.module';
 import { NoctuaInlineEditorComponent } from './inline-editor/inline-editor.component';
 import { NoctuaEditorDropdownComponent } from './inline-editor/editor-dropdown/editor-dropdown.component';
 import { NoctuaReferenceDropdownComponent } from './inline-reference/reference-dropdown/reference-dropdown.component';
+import { NoctuaWithDropdownComponent } from './inline-with/with-dropdown/with-dropdown.component';
 
 @NgModule({
     declarations: [
         NoctuaInlineEditorComponent,
         NoctuaEditorDropdownComponent,
         NoctuaReferenceDropdownComponent,
+        NoctuaWithDropdownComponent,
     ],
     imports: [
         CommonModule,
@@ -24,10 +25,12 @@ import { NoctuaReferenceDropdownComponent } from './inline-reference/reference-d
     exports: [
         NoctuaInlineEditorComponent,
         NoctuaReferenceDropdownComponent,
+        NoctuaWithDropdownComponent
     ],
     entryComponents: [
         NoctuaEditorDropdownComponent,
         NoctuaReferenceDropdownComponent,
+        NoctuaWithDropdownComponent
     ]
 })
 export class NoctuaEditorModule {
