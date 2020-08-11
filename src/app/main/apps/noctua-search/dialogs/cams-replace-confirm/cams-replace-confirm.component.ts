54667bc6d2fd1e0596d8fbcf7c30eb25a64b0145
tmushayahama <tmushayahama@gmail.com>
2020-08-11 16:06:22 -0700

    adding the summary for replace

--- src/app/main/apps/noctua-search/dialogs/cams-replace-confirm/cams-replace-confirm.component.ts
+++ src/app/main/apps/noctua-search/dialogs/cams-replace-confirm/cams-replace-confirm.component.ts
@@ -21,14 +21,14 @@ import {
   Entity
 } from 'noctua-form-base';
 
-import { takeUntil, distinctUntilChanged } from 'rxjs/operators';
+import { takeUntil, distinctUntilChanged, } from 'rxjs/operators';
 import { NoctuaDataService } from '@noctua.common/services/noctua-data.service';
 import { NoctuaSearchService } from '@noctua.search/services/noctua-search.service';
 import { noctuaAnimations } from '@noctua/animations';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormGroup, FormControl } from '@angular/forms';
 import { NoctuaReviewSearchService } from '@noctua.search/services/noctua-review-search.service';
-import { groupBy } from 'lodash';
+import { groupBy, each, find } from 'lodash';
 
 @Component({
   selector: 'noc-cams-replace-confirm-dialog',
@@ -58,20 +58,24 @@ export class CamsReplaceConfirmDialogComponent implements OnInit, OnDestroy {
       public noctuaAnnotonFormService: NoctuaAnnotonFormService,
       public noctuaFormMenuService: NoctuaFormMenuService) {
     this._unsubscribeAll = new Subject();
-
-
-
-
   }
 
   ngOnInit(): void {
     const self = this;
 
-    this.groupedEntities = groupBy(
-      this.noctuaReviewSearchService.matchedEntities, 'modelId') as { string: Entity[] };
+    const groupedEntities = groupBy(this._data.entities, 'modelId') as { string: Entity[] };
 
     this.models = Object.keys(this.groupedEntities).length;
     this.occurrences = this.noctuaReviewSearchService.matchedCount;
+    const promises = []
+    each(groupedEntities, (values: Entity[], key) => {
+      const cam: Cam = find(this.noctuaReviewSearchService.cams, { modelId: key });
+
+      promises.push({
+        cam,
+        values
+      });
+    });
   }
 
   ngOnDestroy(): void {
@@ -79,7 +83,7 @@ export class CamsReplaceConfirmDialogComponent implements OnInit, OnDestroy {
     this._unsubscribeAll.complete();
   }
 
-  replace() {
+  save() {
     this._matDialogRef.close(true);
   }
 
