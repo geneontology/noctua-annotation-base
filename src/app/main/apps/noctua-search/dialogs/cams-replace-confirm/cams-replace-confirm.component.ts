6177cd33d0b6bd310384bac1c169238d4e98fa56
tmushayahama <tmushayahama@gmail.com>
2020-08-27 11:11:38 -0700

    removed the link to nodes

--- src/app/main/apps/noctua-search/dialogs/cams-replace-confirm/cams-replace-confirm.component.ts
+++ src/app/main/apps/noctua-search/dialogs/cams-replace-confirm/cams-replace-confirm.component.ts
@@ -21,14 +21,14 @@ import {
   Entity
 } from 'noctua-form-base';
 
-import { takeUntil, distinctUntilChanged, } from 'rxjs/operators';
+import { takeUntil, distinctUntilChanged } from 'rxjs/operators';
 import { NoctuaDataService } from '@noctua.common/services/noctua-data.service';
 import { NoctuaSearchService } from '@noctua.search/services/noctua-search.service';
 import { noctuaAnimations } from '@noctua/animations';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormGroup, FormControl } from '@angular/forms';
 import { NoctuaReviewSearchService } from '@noctua.search/services/noctua-review-search.service';
-import { groupBy, each, find } from 'lodash';
+import { groupBy } from 'lodash';
 
 @Component({
   selector: 'noc-cams-replace-confirm-dialog',
@@ -63,19 +63,13 @@ export class CamsReplaceConfirmDialogComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     const self = this;
 
-    const groupedEntities = groupBy(this._data.entities, 'modelId') as { string: Entity[] };
+    this.groupedEntities = groupBy(
+      this.noctuaReviewSearchService.matchedEntities, 'modelId') as { string: Entity[] };
 
     this.models = Object.keys(this.groupedEntities).length;
     this.occurrences = this.noctuaReviewSearchService.matchedCount;
-    const promises = []
-    each(groupedEntities, (values: Entity[], key) => {
-      const cam: Cam = find(this.noctuaReviewSearchService.cams, { modelId: key });
 
-      promises.push({
-        cam,
-        values
-      });
-    });
+
   }
 
   ngOnDestroy(): void {
