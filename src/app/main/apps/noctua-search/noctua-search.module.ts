e6a64ffda9efade0f928d1f8235711a32bc77598
tmushayahama <tmushayahama@gmail.com>
2020-10-26 17:11:59 -0700

    Deleted delete button and connect activity https://github.com/geneontology/noctua-form/issues/121

--- src/app/main/apps/noctua-search/noctua-search.module.ts
+++ src/app/main/apps/noctua-search/noctua-search.module.ts
@@ -16,7 +16,7 @@ import { CamsReviewChangesComponent } from './cams/cams-review-changes/cams-revi
 
 const routes = [
   {
-    path: '',
+    path: 'f',
     component: NoctuaSearchComponent
   }
 ];
