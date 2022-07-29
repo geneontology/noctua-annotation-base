5497f5ecd02edaa0b4e88c4d05fb15d2f041564c
tmushayahama <tmushayahama@gmail.com>
2022-07-28 17:43:48 -0700

    no announcement banner

--- scripts/git-push.sh
+++ scripts/git-push.sh
@@ -1,13 +1,13 @@
 set -e
 cd ../noctua-form
 git add --all
-git commit -m "fix evidence issue has_input, added announcement banner sample"
+git commit -m "fix evidence issue has_input, and happends during"
 git push origin sprint-1
 cd ../noctua-landing-page
 git add --all
-git commit -m "fix evidence issue has_input, added announcement banner sample"
+git commit -m "fix evidence issue has_input, and happends during"
 git push origin sprint-1
 cd ../noctua-visual-pathway-editor
 git add --all
-git commit -m "fix evidence issue has_input, added announcement banner sample"
+git commit -m "fix evidence issue has_input, and happends during"
 git push origin sprint-1
