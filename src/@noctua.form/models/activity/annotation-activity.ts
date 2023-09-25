14d02d79eb9eedc3ee85ea845e96c4df69d545cf
tmushayahama <tmushayahama@gmail.com>
2023-09-25 10:04:15 -0700

    Adding some cc and bb logic

--- src/@noctua.form/models/activity/annotation-activity.ts
+++ src/@noctua.form/models/activity/annotation-activity.ts
@@ -4,6 +4,7 @@ import { noctuaFormConfig } from './../../noctua-form-config';
 import { Activity } from './activity';
 import { Triple } from './triple';
 import { Predicate } from './predicate';
+import * as EntityDefinition from './../../data/config/entity-definition';
 
 
 export class AnnotationActivity {
@@ -36,26 +37,62 @@ export class AnnotationActivity {
     const saveData = {
       title: 'enabled by ' + self.gp?.term.label,
       triples: [],
-      nodes: [],
+      nodes: [self.gp, self.goterm],
       graph: null
     };
 
-    const gpTpTermTriple = new Triple(self.gp, self.goterm,
-      new Predicate(this.gpToTermEdge, self.goterm.predicate.evidence));
+    if (this.gpToTermEdge.id === noctuaFormConfig.inverseEdge.enables.id) {
+      const gpToTermTriple = new Triple(self.goterm, self.gp,
+        new Predicate(Entity.createEntity(noctuaFormConfig.edge.enabledBy), self.goterm.predicate.evidence));
 
-    const extensionTriple = new Triple(self.goterm, self.extension,
-      new Predicate(this.extensionEdge, self.goterm.predicate.evidence));
+      saveData.triples.push(gpToTermTriple);
+
+    } else if (this.gpToTermEdge.id === noctuaFormConfig.inverseEdge.involvedIn.id) {
+      const mfNode = EntityDefinition.generateBaseTerm([]);
+      const rootMF = noctuaFormConfig.rootNode.mf;
+
+      mfNode.term = new Entity(rootMF.id, rootMF.label);
+
+      const mfToGpTriple = new Triple(mfNode, self.gp,
+        new Predicate(Entity.createEntity(noctuaFormConfig.edge.enabledBy), self.goterm.predicate.evidence));
+
+      const mfToTermTriple = new Triple(mfNode, self.goterm,
+        new Predicate(Entity.createEntity(noctuaFormConfig.edge.partOf), self.goterm.predicate.evidence));
+
+      saveData.triples.push(mfToGpTriple);
+      saveData.triples.push(mfToTermTriple);
+
+    } else if (this.gpToTermEdge.id === noctuaFormConfig.inverseEdge.isActiveIn.id) {
+      const mfNode = EntityDefinition.generateBaseTerm([]);
+      const rootMF = noctuaFormConfig.rootNode.mf;
+
+      mfNode.term = new Entity(rootMF.id, rootMF.label);
+
+      const mfToGpTriple = new Triple(mfNode, self.gp,
+        new Predicate(Entity.createEntity(noctuaFormConfig.edge.enabledBy), self.goterm.predicate.evidence));
+
+      const mfToTermTriple = new Triple(mfNode, self.goterm,
+        new Predicate(Entity.createEntity(noctuaFormConfig.edge.occursIn), self.goterm.predicate.evidence));
+
+      saveData.triples.push(mfToGpTriple);
+      saveData.triples.push(mfToTermTriple);
+
+    }
 
 
-    saveData.nodes = [self.gp, self.goterm, self.extension];
-    saveData.triples = [gpTpTermTriple, extensionTriple];
 
 
+    if (self.extension?.hasValue()) {
+      const extensionTriple = new Triple(self.goterm, self.extension,
+        new Predicate(this.extensionEdge, self.goterm.predicate.evidence));
 
-    // saveData.triples = 
+      saveData.nodes.push(self.extension);
+      saveData.triples.push(extensionTriple);
+    }
 
     return saveData;
   }
+
 
   updateAspect() {
     if (!this.goterm.hasValue()) return
