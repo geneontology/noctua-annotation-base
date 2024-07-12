8e755b802cb5bfc4798e377379c6073eaca533c0
tmushayahama <tmushayahama@gmail.com>
2024-07-12 11:31:07 -0700

    Added no info and comporessed the table a bit

--- scripts/build-nsa.sh
+++ scripts/build-nsa.sh
@@ -5,18 +5,7 @@ cd ../noctua-standard-annotations
 rm -rf src
 cp -r ../noctua-form-base/src .
 
-ignoreFiles=("src/app/layout/components/toolbar/toolbar.component.html"
-"src/app/main/apps/noctua-form/noctua-form.module.ts"
-"src/app/main/apps/noctua-annotations/noctua-annotations.module.ts"
-"src/app/main/apps/noctua-search/noctua-search.module.ts"
-"src/app/main/apps/noctua-graph/noctua-graph.module.ts"
-"src/environments/environment.ts"
-"src/environments/environment.prod.ts"
-"src/environments/environment.beta-test.ts")
 
-for str in ${ignoreFiles[@]}; do
-   git checkout $str
-done
 
 rm -rf workbenches/noctua-standard-annotations/public
 npm run build
