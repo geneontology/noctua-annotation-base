cb92c6c4640d74f765b0b24e2be4cca02aab6ad0
tmushayahama <tmushayahama@gmail.com>
2024-12-16 20:50:05 -0800

    updated the pathway viewer

--- scripts/build-appr.sh
+++ scripts/build-appr.sh
@@ -5,7 +5,8 @@ cd ../noctua-alliance-pathway-preview
 rm -rf src
 cp -r ../noctua-form-base/src .
 
-ignoreFiles=("src/app/layout/components/toolbar/toolbar.component.html"
+ignoreFiles=(
+#"src/app/layout/components/toolbar/toolbar.component.html"
 #"src/app/main/apps/noctua-form/noctua-form.module.ts"
 #"src/app/main/apps/noctua-annotations/noctua-annotations.module.ts"
 #"src/app/main/apps/noctua-search/noctua-search.module.ts"
@@ -19,4 +20,4 @@ for str in ${ignoreFiles[@]}; do
 done
 
 rm -rf workbenches/noctua-alliance-pathway-preview/public
-npm run build
+#npm run build
